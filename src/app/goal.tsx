1eb68dacfb5998482ac230a4663f916a05662324
veidzvj <joaovictorveidz@gmail.com>
2025-09-03 16:53:52 -0300

    feat: add Input

--- src/app/goal.tsx
+++ src/app/goal.tsx
@@ -1,13 +1,17 @@
+import { View } from "react-native"
+import { Button } from "@/components/Button"
+import { Input } from "@/components/Input"
 import { PageHeader } from "@/components/PageHeader"
-import { router } from "expo-router"
-import { Button, View } from "react-native"
 
 export default function Goal() {
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <PageHeader title="Meta" subtitle="Economize para alcançar sua meta financeira" />
 
-      <Button title="Voltar" onPress={() => router.back()} />
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <Input label="Nome da meta" placeholder="Ex: Viagem para praia, Apple Watch" />
+        <Button title="Salvar" />
+      </View>
     </View>
   )
 }
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,17 @@
+import { Text, TextInput, TextInputProps, View } from "react-native"
+import { styles } from "./styles"
+import { colors } from "@/theme"
+
+type Props = TextInputProps & {
+  label: string
+}
+
+export function Input({ label, ...rest }: Props) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+
+      <TextInput style={styles.input} placeholderTextColor={colors.gray[400]} {...rest} />
+    </View>
+  )
+}
